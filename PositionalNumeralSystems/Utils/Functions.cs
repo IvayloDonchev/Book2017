a2317c64ce83d8a5ee23161974b10ab261c9b71a
Ivaylo Donchev <idonchev@hotmail.com>
2019-02-02 21:33:06 +0200

    DLL added

---
+++ PositionalNumeralSystems/Utils/Functions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class Functions
+    {
+        public static int Pow(int a, int b)
+        {
+            int res = 1;
+            for (int i = 1; i <= b; i++)
+                res *= a;
+            return res;
+        }
+        public static string FromDec(int n, int r)  // От десетична в r-ична
+        {
+            string sign = n > 0 ? "" : "-";
+            string s = "";
+            int d;
+            n = Math.Abs(n);
+            while (n != 0)
+            {
+                d = n % r;
+                if (d > 9)
+                    s = (char)(d + 'A' - 10) + s;
+                else
+                    s = (char)(d + '0') + s;
+                n = n / r;
+            }
+            return sign + s;
+        }
+        public static int ToDec(string s, int r)    // От r-ична в десетична
+        {
+            int n = 0;
+            int sign = 1;
+            if (s[0] == '-')
+            {
+                sign = -1;
+                s = s.Substring(1);
+            }
+            int l = s.Length;
+            s = s.ToUpper();
+            for (int i = 0; i < l; i++)
+            {
+                if (s[i] >= 'A')
+                    n = n + (s[i] - 'A' + 10) * Pow(r, l - i - 1);
+                else
+                    n = n + (s[i] - '0') * Pow(r, l - i - 1);
+            }
+            return sign * n;
+        }
+    }
+}

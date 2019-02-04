05e6909d589e831c3c585775345b1905819d58ba
Ivaylo Donchev <idonchev@hotmail.com>
2019-02-04 11:07:55 +0200

    df

--- Chapter03/Ch03Ex01/Ch03Ex01/Program.cs
+++ Chapter03/Ch03Ex01/Ch03Ex01/Program.cs
@@ -15,6 +15,19 @@ namespace Ch03Ex01
             myInteger = 17;
             myString = "\"myInteger\" is";
             Console.WriteLine($"{myString} {myInteger}");
+
+            myString = "This string has a\nline break.";
+            Console.WriteLine($"{myString} {myInteger}");
+
+            int[] binaryPhases = { 0b00110001, 0b01100010, 0b11000100, 0b1000100 };
+            int[] hexPhases = { 0x31, 0x62, 0xC4, 0x89 };
+                    
+            int[] numbers = { 0b0010_0000, 0b0100_0000, 0b1000_0000 };
+
+            myString = @"It's a verbatim string
+placed in two lines";
+            Console.WriteLine(myString);
+
             Console.ReadKey();
         }
     }

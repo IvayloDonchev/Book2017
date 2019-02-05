00a72bbdd775a97782d24c3b578c4c4e19b0984d
Ivaylo Donchev <idonchev@hotmail.com>
2019-02-05 15:52:11 +0200

    vb

---
+++ Chapter05/Ch05Ex01/Ch05Ex01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Ch05Ex01
+{
+    class Program
+    {
+        enum Orientation : byte
+        {
+            north = 1,
+            south = 2,
+            east = 3,
+            west = 4
+        }
+        static void Main(string[] args)
+        {
+            byte directionByte;
+            string directionString;
+            Orientation myDirection = Orientation.north;
+            WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            WriteLine($"byte equivalent = {directionByte}");
+            WriteLine($"string equivalent = {directionString}");
+
+            string myString = "north";
+            myDirection = (Orientation)Enum.Parse(typeof(Orientation), myString);
+            ReadKey();
+        }
+    }
+}
